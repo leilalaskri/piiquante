f5f13d65f15e7af2bd2aef1ac6bbfc20249bb224
leilalaskri <95353539+leilalaskri@users.noreply.github.com>
2022-10-02 15:22:41 +0100

    multer

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,2 +1,83 @@
 const mongoose = require('mongoose');
 const Sauce = require("../models/sauce");
+const sauces = new sauces({});
+sauces.save().then(
+() => {
+    res.status(201).json({
+        message: 'Post saved successfully!'
+    });
+}
+).catch(
+(error) => {
+    res.status(400).json({
+        error: error
+    });
+}
+);
+});
+
+router.get('/:id', (req, res, next) => {
+    sauces.findOne({
+        _id: req.params.id
+    }).then(
+        (sauces) => {
+            res.status(200).json(sauces);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+});
+
+router.put('/:id', (req, res, next) => {
+    const sauces = new sauces({
+
+    });
+    sauces.updateOne({ _id: req.params.id }, sauces).then(
+        () => {
+            res.status(201).json({
+                message: 'sauces updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+router.delete('/:id', (req, res, next) => {
+    sauces.deleteOne({ _id: req.params.id }).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+router.get('/' +
+    '', (req, res, next) => {
+        sauces.find().then(
+            (sauces) => {
+                res.status(200).json(sauces);
+            }
+        ).catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
+            }
+        );
+    });
--- routes/sauces.js
+++ routes/sauces.js
@@ -1,89 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 const sauces = require('../models/sauces');
+const saucesCtrl = require('../controllers/sauces');
 
+router.get('/', auth, saucesCtrl.getAllsauces);
+router.post('/', auth, multer, saucesCtrl.createsauces);
+router.get('/:id', auth, saucesCtrl.getOnesauces);
+router.put('/:id', auth, saucesCtrl.modifysauces);
+router.delete('/:id', auth, saucesCtrl.deletesauces);
 router.post('/', (req, res, next) => {
-    const sauces = new sauces({});
-    sauces.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
 
-router.get('/:id', (req, res, next) => {
-    sauces.findOne({
-        _id: req.params.id
-    }).then(
-        (sauces) => {
-            res.status(200).json(sauces);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error
-            });
-        }
-    );
-});
 
-router.put('/:id', (req, res, next) => {
-    const sauces = new sauces({
-
-    });
-    sauces.updateOne({ _id: req.params.id }, sauces).then(
-        () => {
-            res.status(201).json({
-                message: 'sauces updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-router.delete('/:id', (req, res, next) => {
-    sauces.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-router.get('/' +
-    '', (req, res, next) => {
-        Thing.find().then(
-            (things) => {
-                res.status(200).json(things);
-            }
-        ).catch(
-            (error) => {
-                res.status(400).json({
-                    error: error
-                });
-            }
-        );
-    });
-
-module.exports = router;
+            module.exports = router;
