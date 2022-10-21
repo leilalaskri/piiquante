21cf39abdb7171001af43d44dd592a1f5106f463
leilalaskri <95353539+leilalaskri@users.noreply.github.com>
2022-10-21 17:46:58 +0100

    code de reponse

--- controllers/like.js
+++ controllers/like.js
@@ -16,7 +16,7 @@ exports.addLikeOrDislike = (req, res, next) => {
 
                     .then(() => res.status(201).json({ message: "Objet modifié !" }))
                         .catch((error) => res.status(400).json({ error }))
-                } else { res.status(400).json({ message: "utilisateur a deja liké!" }) }
+                } else { res.status(401).json({ message: "utilisateur a deja liké!" }) }
             };
             if (req.body.likes == -1) {
                 if ((!sauce.usersLiked.includes(req.body.userId)) && (!sauce.usersDisliked.includes(req.body.userId))) {
@@ -27,7 +27,7 @@ exports.addLikeOrDislike = (req, res, next) => {
                         })
                         .then(() => res.status(201).json({ message: "Objet modifié !" }))
                         .catch((error) => res.status(400).json({ error }))
-                } else { res.status(400).json({ message: "utilisateur a deja liké!" }) }
+                } else { res.status(401).json({ message: "utilisateur a deja liké!" }) }
             };
             if (req.body.likes == 0) {
                 /////////
@@ -36,7 +36,7 @@ exports.addLikeOrDislike = (req, res, next) => {
                             $inc: { likes: -1 },
                             $pull: { usersLiked: req.body.userId }
                         })
-                        .then(() => res.status(201).json({ message: "utilisateur enleve like !" }))
+                        .then(() => res.status(201).json({ message: "utilisateur a enlevé like !" }))
                         .catch((error) => res.status(400).json({ error }));
                 }
                 //////////////
@@ -45,12 +45,13 @@ exports.addLikeOrDislike = (req, res, next) => {
                             $inc: { dislikes: 1 },
                             $pull: { usersDisliked: req.body.userId }
                         })
-                        .then(() => res.status(201).json({ message: "Objet enleve dislike " }))
+                        .then(() => res.status(201).json({ message: "utilisateur a enlevé dislike " }))
                         .catch((error) => res.status(400).json({ error }))
-                } else { res.status(400).json({ message: "utilisateur n'a pas liké ou disliké" }) }
+                } else { res.status(401).json({ message: "utilisateur n'a pas liké ou disliké" }) }
             };
             if ((req.body.likes != -1) || (req.body.likes != 1) || (req.body.likes != 0)) {
                 res.status(400).json({ message: "operation non autorisé" })
             };
-        });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
